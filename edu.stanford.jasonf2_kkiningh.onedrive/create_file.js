d21be8e85d2910dfcb0ea8d3a45c744f9620ef0e
Jason Freeman <jfreeman@microsoft.com>
2016-03-06 20:55:03 -0800

    Add create_file action

---
+++ edu.stanford.jasonf2_kkiningh.onedrive/create_file.js
@@ -0,0 +1,24 @@
+const Tp = require('thingpedia');
+
+module.exports = new Tp.ChannelClass({
+        Name: 'OneDriveCreateFileAction',
+        Extends: Tp.SimpleAction,
+
+        _init: function(engine, device) {
+            this.parent();
+            this.device = device;
+            this._baseurl = 'https://api.onedrive.com/v1.0/drive/root/children/';
+        },
+
+        get auth() {
+            return "Bearer " + this.device.accessToken;
+        },
+
+        _doInvoke: function(fileName, body) {
+            var url = this._baseurl + fileName + "/content";
+            Tp.Helpers.Http.request(url, 'PUT', body, {
+                dataContentType: "text/plain",
+                auth: this.auth
+            }).done();
+        }
+    });
